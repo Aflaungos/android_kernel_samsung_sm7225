d598ca4a453b3979384fd8a45285b73f4eef5d99
Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>
2024-01-24 14:28:35 -0300

    kernel: boost cpu when launching an app
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -95,6 +95,7 @@
 #include <linux/thread_info.h>
 #include <linux/cpufreq_times.h>
 #include <linux/scs.h>
+#include <linux/cpu_input_boost.h>
 #include <linux/task_integrity.h>
 
 #include <asm/pgtable.h>
@@ -2517,6 +2518,11 @@ long _do_fork(unsigned long clone_flags,
 	struct task_struct *p;
 	int trace = 0;
 	long nr;
+
+	/* Boost DDR bus to the max for 50 ms when userspace launches an app */
+	if (task_is_zygote(current)) {
+		cpu_input_boost_kick_max(50);
+	}
 
 	/*
 	 * Determine whether and which event to report to ptracer.  When
