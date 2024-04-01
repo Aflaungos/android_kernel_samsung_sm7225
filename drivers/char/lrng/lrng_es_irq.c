06470a51b1f3eccf31e7de19fb3daf5f43a26268
Aflaungos <vimavick@gmail.com>
2024-04-01 00:26:50 -0300

    Revert "lrng: merge v45 from linux-5.15.y"
    This reverts commit f3ed641669a988abe76da8b1bf469610c552d14c.

--- drivers/char/lrng/lrng_es_irq.c
+++ drivers/char/lrng/lrng_es_irq.c
@@ -14,6 +14,7 @@
 #include <linux/gcd.h>
 #include <linux/module.h>
 #include <linux/random.h>
+#include <linux/ratelimit.h>
 
 #include "lrng_es_aux.h"
 #include "lrng_es_irq.h"
@@ -658,7 +659,7 @@ static void lrng_time_process(void)
 }
 
 /* Hot code path - Callback for interrupt handler */
-void add_interrupt_randomness(int irq)
+void add_interrupt_randomness(int irq, int irq_flg)
 {
 	if (lrng_highres_timer()) {
 		lrng_time_process();
@@ -692,6 +693,7 @@ void add_interrupt_randomness(int irq)
 		tmp = lrng_raw_jiffies_entropy_store(jiffies) ? 0 : jiffies;
 		tmp ^= lrng_raw_irq_entropy_store(irq) ? 0 : irq;
 		tmp ^= lrng_raw_retip_entropy_store(ip) ? 0 : ip;
+		tmp ^= irq_flg;
 		tmp ^= ip >> 32;
 		_lrng_irq_array_add_u32(tmp);
 	}
