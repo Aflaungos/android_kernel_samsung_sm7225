599f3965bf4662a91192abdad9d38bca94108918
Aflaungos <vimavick@gmail.com>
2024-05-15 11:38:51 -0300

    Revert "Merge tag 'v4.19.292' of https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux"
    This reverts commit 2af7d25ef0b879d1c064693af112a4e7ec78089c.

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -6439,7 +6439,6 @@ err_init_binder_device_failed:
 
 err_alloc_device_names_failed:
 	debugfs_remove_recursive(binder_debugfs_dir_entry_root);
-	binder_alloc_shrinker_exit();
 
 err_workqueue_init_failed:
 	destroy_workqueue(binder_deferred_workqueue);
