a4f74f80997162b0fd668a0f982c1d3ea50e0d55
Aflaungos <vimavick@gmail.com>
2024-05-16 10:56:40 -0300

    Revert "Merge tag 'v4.19.279' of https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux into 13.0-lmi"
    This reverts commit 70c0a25496c3832b0ee48350b0301ba0c9fc9d89.

--- drivers/gpu/drm/i915/intel_ringbuffer.c
+++ drivers/gpu/drm/i915/intel_ringbuffer.c
@@ -1132,11 +1132,10 @@ static struct i915_vma *
 intel_ring_create_vma(struct drm_i915_private *dev_priv, int size)
 {
 	struct i915_address_space *vm = &dev_priv->ggtt.vm;
-	struct drm_i915_gem_object *obj = NULL;
+	struct drm_i915_gem_object *obj;
 	struct i915_vma *vma;
 
-	if (!HAS_LLC(dev_priv))
-		obj = i915_gem_object_create_stolen(dev_priv, size);
+	obj = i915_gem_object_create_stolen(dev_priv, size);
 	if (!obj)
 		obj = i915_gem_object_create_internal(dev_priv, size);
 	if (IS_ERR(obj))
